e932d0a3319e76ab14deb1f203b85a43f434c4c7
Juan Beltrame <juan_beltrame@hotmail.com>
2023-11-10 19:15:05 -0300

    Update

---
+++ 00.1 name of/00.1 name of/nameOf_Practice.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Channels;
+
+namespace _00._1_name_of
+{
+    public static class nameOf_Practice
+    {
+        static string miVariable = "Hola, Mundo";
+
+        public static string DevolverNameOf()
+        {
+            return (nameof(miVariable));
+        }
+    }
+}
--- Using Keywords/Program.cs
+++ Using Keywords/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _00._1_name_of;
+
+string valorRecibido = nameOf_Practice.DevolverNameOf();
+Console.WriteLine(valorRecibido);
