6e755c59c03ca11677d58f280bcaeeecb57704fd
Juan Beltrame <juan_beltrame@hotmail.com>
2023-12-23 19:44:24 -0300

    Update

--- record - Practice 01 (View)/Program.cs
+++ record - Practice 01 (View)/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+using System.Reflection.Metadata;
+
+
+Date date = new Date(2023, 2, 14);
+
+var monthName = date.GetMonthName();
+Console.WriteLine(monthName);
+
+record Date(int Year, int Month, int Day)
+{
+    public string GetMonthName()
+    {
+        return DateTimeFormatInfo.CurrentInfo.GetMonthName(Month);
+    }
+}
+
