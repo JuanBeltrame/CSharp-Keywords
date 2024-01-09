a0a5920e4210e66ff918dcd7ef93c20b16055576
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2024-01-09 15:51:17 -0300

    Update

--- var - Practice 01/Program.cs
+++ var - Practice 01/Program.cs
@@ -1,5 +1,13 @@
 ﻿using System.Reflection.Metadata;
 
+// Regular Sintax
 UserStringHandle user01 = new UserStringHandle();
 
+// var Sintax
 var user02 = new UserStringHandle();
+
+// New Sintax
+CancellationToken cancellationToken = new();
+
+// New Sintax
+List<int> numeros = [];
